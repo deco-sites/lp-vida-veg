476702cd2953bb5c38dceb10379fb7f755b6819a
Wesley Moraes <wesleymoraesserafim@gmail.com>
2025-02-18 15:08:19 +0000

    Publish 2 from main into origin/main

--- sections/BtnLink.tsx
+++ sections/BtnLink.tsx
@@ -15,7 +15,7 @@ const LinkButton = ({
             href={link}
             target="_blank"
             rel="noopener noreferrer"
-            class={` fixed bottom-16 right-4 z-[100] flex items-center bg-green-500 text-white px-4 py-2 rounded shadow-lg`}
+            class={` fixed bottom-18 right-4 z-[100] flex items-center bg-green-500 text-white px-4 py-2 rounded shadow-lg`}
         >
             {buttonText}
         </a>
